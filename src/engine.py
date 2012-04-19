c057a566032ad9e4fcd617bb18902c60790a78dd
Christopher Rogers <chrissrogers@gmail.com>
2012-04-18 23:16:06 -0500

    Small layout changes and simplifying GAE wsgi loader
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- src/engine.py
+++ src/engine.py
@@ -19,19 +19,3 @@ for filename in os.listdir(package_dir_path):
     sys.path.insert(0, "%s/%s" % (package_dir_path, filename))
 
 from etherous import app
-
-
-def start (url_mapping, debug = False):
-  from google.appengine.ext.webapp.util import run_wsgi_app
-  if app.debug:
-    # Run debugged app
-    from werkzeug_debugger_appengine import get_debugged_app
-    run_wsgi_app(get_debugged_app(app))
-  else:
-    # Run production app
-    run_wsgi_app(app)
-
-
-# Use App Engine app caching
-if __name__ == "__main__":
-  start()
