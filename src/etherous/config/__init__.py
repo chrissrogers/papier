f2af15df1b32ba5a8b9264ed6733e13df2a4b3e0
Christopher Rogers <chrissrogers@gmail.com>
2012-04-21 21:38:27 -0500

    config realignment and asset watcher improvements
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- src/etherous/config/__init__.py
+++ src/etherous/config/__init__.py
@@ -9,8 +9,16 @@
 
 import os
 
+from secret_keys import CSRF_SECRET_KEY, SESSION_KEY
+
+# Set secret keys for CSRF protection
+SECRET_KEY = CSRF_SECRET_KEY
+CSRF_SESSION_KEY = SESSION_KEY
+
+CSRF_ENABLED = True
+
 # Auto-set debug mode based on App Engine dev environ
 if 'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-  import development
+  DEBUG = True
 else:
-  import production
+  DEBUG = False
---
+++ src/etherous/static/assets/js/application.dev.js
@@ -0,0 +1,60 @@
+(function (global, main, modules, sandboxed_modules) {
+    var initialized_modules = {},
+        require = function (moduleName) {
+            var module = modules[moduleName],
+                output;
+
+            // Already inited - return as is
+            if (initialized_modules[moduleName] && module) {
+                return module;
+            }
+
+            // Lazy LMD module
+            if (typeof module === "string") {
+                module = window.eval(module);
+            }
+
+            // Predefine in case of recursive require
+            output = {exports: {}};
+            initialized_modules[moduleName] = 1;
+            modules[moduleName] = output.exports;
+
+            if (!module) {
+                // if undefined - try to pick up module from globals (like jQuery)
+                module = global[moduleName];
+            } else if (typeof module === "function") {
+                // Ex-Lazy LMD module or unpacked module ("pack": false)
+                module = module(sandboxed_modules[moduleName] ? null : require, output.exports, output) || output.exports;
+            }
+
+            return modules[moduleName] = module;
+        },
+        output = {exports: {}};
+
+    for (var moduleName in modules) {
+        // reset module init flag in case of overwriting
+        initialized_modules[moduleName] = 0;
+    }
+
+    main(require, output.exports, output);
+})(this,(function (require, exports, module) { /* wrapped by builder */
+
+require.config({
+  baseUrl: '/assets/js',
+  paths: {
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min',
+    ember: 'lib/ember-require',
+    _: 'lib/underscore'
+  }
+});
+
+require(['app/init'], function (etherous) {
+
+
+
+});
+
+
+}),{
+
+},{})
