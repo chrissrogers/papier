367ac7ffbd78da89bdbe85331f60793287aadac0
Christopher Rogers <chrissrogers@gmail.com>
2012-04-24 13:08:14 -0500

    moving source assets out of static folder
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- src/etherous/assets/scss/config.rb
+++ src/etherous/assets/scss/config.rb
@@ -3,10 +3,10 @@ require 'animation'
  
 # Set this to the root of your project when deployed:
 http_path = '../../'
-css_dir = '../css'
+css_dir = '../../static/assets/css'
 sass_dir = './'
-images_dir = '../img'
-javascripts_dir = '../js'
+images_dir = '../../static/assets/img'
+javascripts_dir = '../../static/assets/js'
  
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
