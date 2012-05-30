9ed09b5943179f8a0726e42092170906c505059c
Christopher Rogers <chrissrogers@gmail.com>
2012-05-30 17:46:26 -0500

    adds template actions, spacing
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- src/application/assets/js/app/views/editor.js
+++ src/application/assets/js/app/views/editor.js
@@ -1,8 +1,8 @@
   
 papier.EditorView = Ember.View.create({
+
   template: Ember.Handlebars.compile(require('template-editor')),
-  president: papier.president.name,
-  mouseDown: function () {
-    window.alert("Editor");
-  }
+  
+  president: papier.president.name
+
 });
--- src/application/assets/js/app/views/welcome.js
+++ src/application/assets/js/app/views/welcome.js
@@ -1,4 +1,12 @@
 
 papier.WelcomeView = Ember.View.create({
+  
   template: Ember.Handlebars.compile(require('template-welcome')),
+
+  launchEditor: function (event) {
+
+    $(this).css('border', '10px solid red');
+
+  }
+
 });
